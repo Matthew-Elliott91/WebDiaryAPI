e1092f8b6bc646c0ee0077440aa0b2cc908b45c7
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-23 21:57:33 +0000

    Added HTTP Put and Delete for diary api.

--- WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.EntityFrameworkCore;
 using WebDiaryAPI.Data;
 using WebDiaryAPI.Models;
@@ -36,6 +37,68 @@ namespace WebDiaryAPI.Controllers
             return diaryEntry;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<DiaryEntry>> PostDiaryEntry(DiaryEntry diaryEntry)
+        {
+            diaryEntry.Id = 0;
+            _context.DiaryEntries.Add(diaryEntry);
+            await _context.SaveChangesAsync();
+            var resourceURL = Url.Action(nameof(GetDiaryEntry), new { id = diaryEntry.Id });
+            return Created(resourceURL, diaryEntry);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult<DiaryEntry>> PutDiaryEntry([FromBody] DiaryEntry diaryEntry, int id)
+        {
+            if (id != diaryEntry.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _context.Entry(diaryEntry).State = EntityState.Modified;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiaryEntryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+
+
+        }
+        private bool DiaryEntryExists(int id)
+        {
+            return _context.DiaryEntries.Any(e => e.Id == id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DiaryEntry>> DeleteDiaryEntry(int id)
+        {
+            DiaryEntry diaryEntry = await _context.DiaryEntries.FindAsync(id);
+
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.DiaryEntries.Remove(diaryEntry);
+                await _context.SaveChangesAsync();
+            }
+
+            return NoContent();
+        }
 
         //Another method that is synchronous
 
